93dd9ed263bdec9f0e6ac54aed63a1971bbb8ad8
Clement Zheng <clement.zheng@gmail.com>
2019-07-09 19:42:56 -0600

    parameter edit 7-9-2019

--- test.py
+++ test.py
@@ -2,10 +2,10 @@ import cv2
 import numpy as np
 from cv2 import aruco
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 cap.set(cv2.CAP_PROP_FPS, 50)
-# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 dictionary_name = aruco.DICT_4X4_50
 dictionary = aruco.getPredefinedDictionary(dictionary_name)
@@ -14,11 +14,11 @@ parameters = aruco.DetectorParameters_create()
 # Thresholding
 parameters.adaptiveThreshWinSizeMin = 3 # >= 3
 parameters.adaptiveThreshWinSizeStep = 2 # 10
-parameters.adaptiveThreshWinSizeMax = 14 # 23
+parameters.adaptiveThreshWinSizeMax = 10 # 23
 parameters.adaptiveThreshConstant = 7 # 7
 # Contour Filtering
-parameters.minMarkerPerimeterRate = 0.03 # 0.03
-parameters.maxMarkerPerimeterRate = 0.1 # 4.0
+parameters.minMarkerPerimeterRate = 0.05 # 0.03
+parameters.maxMarkerPerimeterRate = 0.5 # 4.0
 parameters.minCornerDistanceRate = 0.05 # 0.05
 parameters.minMarkerDistanceRate = 0.05 # 0.05
 parameters.minDistanceToBorder = 3 # 3
@@ -28,7 +28,7 @@ parameters.minOtsuStdDev = 5.0 # 5.0
 parameters.perspectiveRemoveIgnoredMarginPerCell = 0.4 # 0.13
 # parameters.perpectiveRemovePixelPerCell = 10 # 4
 # Marker Identification
-parameters.maxErroneousBitsInBorderRate = 0.5 # 0.35
+parameters.maxErroneousBitsInBorderRate = 0.8 # 0.35
 parameters.errorCorrectionRate = 1.2 # 0.6
 
 
@@ -41,7 +41,7 @@ print(w, h)
 while True:
   ret, frame = cap.read()
 
-  frame = cv2.addWeighted(frame, 1.3, np.zeros(frame.shape, frame.dtype), 0, 0)
+  # frame = cv2.addWeighted(frame, 1.3, np.zeros(frame.shape, frame.dtype), 0, 0)
 
   # lab = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
   # l, a, b = cv2.split(lab)
@@ -56,7 +56,7 @@ while True:
   # v = clahe.apply(v)
   # frame = cv2.merge((hue,s,v))
   # frame = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)
-
+  
   corners, ids, rejectedImgPoints = aruco.detectMarkers(frame, dictionary, parameters=parameters)
   frame = aruco.drawDetectedMarkers(frame, corners, ids, borderColor=(0, 0, 255))
   frame = aruco.drawDetectedMarkers(frame, rejectedImgPoints, borderColor=(0, 255, 0))
