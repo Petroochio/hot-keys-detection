d529b74f4e57a74b8ae9a2398ca7a27eb56e2233
Clement Zheng <clement.zheng@gmail.com>
2019-08-27 15:54:39 -0600

    integrated inputs

--- preview/CarDemo/index.js
+++ preview/CarDemo/index.js
@@ -1,11 +1,45 @@
 import DrawTarget from './DrawTarget';
+import { initMarkers } from '../InputGenerator/Markers';
+import { pointInRect } from '../InputGenerator/Utils/CollisionDetection';
+import { avgCorners } from '../InputGenerator/Utils/General';
+import { checkPerspective, relativePosition } from '../InputGenerator/Editor/RelativePos';
+import InputGroup from '../InputGenerator/Editor/InputGroup';
+
 let prevTime = 0;
 let zoom = 1000;
 let frameParent;
 
+let socket;
+let markerData;
+let dummyCanvas, dummyCtx;
+
+let inputGroupData = [];
+function initTableInput(inputArr) {
+  inputGroupData = inputArr.groups.map((i) => (new InputGroup(markerData, i)));
+  console.log(inputGroupData);
+}
+
 function update() {
   const timenow = Date.now();
   const dt = timenow - prevTime;
+
+  markerData.forEach(m => m.checkPresence(timenow));
+  inputGroupData.forEach(i => i.update());
+
+  inputGroupData.forEach(i => {
+    switch(i.name) {
+      case 'Panel1A':
+        break;
+      case 'Panel2A':
+        break;
+      case 'Panel3A':
+        break;
+      case 'Panel2B':
+        break;
+      default:
+        break;
+    }
+  });
 
   prevTime = timenow;
 
@@ -14,6 +48,12 @@ function update() {
 
 function init() {
   frameParent = document.querySelector('#frame-parent');
+
+  dummyCanvas = document.querySelector('#dummy-canvas');
+  dummyCtx = dummyCanvas.getContext('2d');
+  dummyCtx.translate(0.5, 0.5);
+  markerData = initMarkers(dummyCtx);
+
   const adjustZoom = (change) => {
     zoom += change;
     frameParent.style.perspective = zoom + 'px';
@@ -23,6 +63,44 @@ function init() {
   const frame3 = new DrawTarget('#wireframe3', adjustZoom);
 
   window.requestAnimationFrame(update.bind(this));
+
+  socket = io.connect('localhost:5000');
+
+  socket.on('connect', () => {
+    console.log('connected to server');
+    socket.emit('get inputs config')
+  });
+
+  socket.on('send inputs config', ({ config }) => initTableInput(JSON.parse(config)));
+
+  socket.on('update markers', (data) => {
+    const markers = data.markers;
+
+    if (markers.length > 0) {
+      const mappedMarkers = markers.map(m => {
+        // include naive conversion here in library
+        const mappedCorners = m.corners
+          .map(c => ({ x: c[0] / 1280, y: c[1] / 720 }));
+
+        return {
+          id: m.id,
+          corner: mappedCorners[0],
+          center: avgCorners(mappedCorners),
+          allCorners: mappedCorners,
+        };
+      });
+
+      // HERE IS WHERE THE ARRAY OF MARKERS IS CLEMENT
+      // MappedMarkers
+      const timenow = Date.now();
+
+      mappedMarkers.forEach(m => {
+        if (m !== undefined) {
+          markerData[m.id].update(m, timenow);
+        }
+      });
+    }
+  });
 }
 
 window.onload = () => init();
